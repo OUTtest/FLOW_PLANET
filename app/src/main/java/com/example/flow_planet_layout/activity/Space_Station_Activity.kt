8d6157e43c8bc7dbd342e7a987fbba724e40faf3
kjy00302 <kjy00302@gmail.com>
2022-06-05 18:25:53 +0900

    move activity classes to activity package

--- app/src/main/java/com/example/flow_planet_layout/activity/Space_Station_Activity.kt
+++ app/src/main/java/com/example/flow_planet_layout/activity/Space_Station_Activity.kt
@@ -1,9 +1,10 @@
-package com.example.flow_planet_layout
+package com.example.flow_planet_layout.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.flow_planet_layout.R
 
 class Space_Station_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
