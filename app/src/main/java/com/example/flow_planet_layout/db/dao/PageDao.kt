0cf0b2dd328226fd07642ac1cd04e7f75694c719
kjy00302 <kjy00302@gmail.com>
2022-06-07 11:26:31 +0900

    remove fts4

--- app/src/main/java/com/example/flow_planet_layout/db/dao/PageDao.kt
+++ app/src/main/java/com/example/flow_planet_layout/db/dao/PageDao.kt
@@ -6,11 +6,8 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PageDao {
-    @Query("SELECT rowid, * FROM pages WHERE bookId = :bookId")
+    @Query("SELECT * FROM pages WHERE bookId = :bookId")
     fun getAllInBook(bookId: Int): Flow<List<Page>>
-
-//    @Query("SELECT * FROM pages WHERE data MATCH :s")
-//    fun search(s: String): Flow<List<Page>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun put(page: Page)
--- app/src/main/java/com/example/flow_planet_layout/db/entity/Page.kt
+++ app/src/main/java/com/example/flow_planet_layout/db/entity/Page.kt
@@ -1,15 +1,12 @@
 package com.example.flow_planet_layout.db.entity
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Fts4
 import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
-@Fts4
 @Entity(tableName = "pages")
 data class Page (
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name="rowid") val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val time: LocalDateTime,
     val bookId: Int,
     var title: String,
