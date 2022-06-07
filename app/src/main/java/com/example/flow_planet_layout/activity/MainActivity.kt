bb92c3b0be37f693b5d5fbb43ebf2ff34fb42461
kjy00302 <kjy00302@gmail.com>
2022-06-07 21:18:47 +0900

    use proper menu drawer

--- app/src/main/java/com/example/flow_planet_layout/activity/MainActivity.kt
+++ app/src/main/java/com/example/flow_planet_layout/activity/MainActivity.kt
@@ -4,19 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.flow_planet_layout.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val Btn_MAIN_Menu = findViewById<Button>(R.id.Btn_MAIN_Menu)
         val Btn_MAIN_Start = findViewById<Button>(R.id.Btn_MAIN_Start)
+        drawerLayout = findViewById(R.id.drawer_main)
+        navView = findViewById(R.id.navview_main)
 
         Btn_MAIN_Menu.setOnClickListener{
-            val intent = Intent(this, MenuActivity_::class.java)
-            startActivity(intent)
+            drawerLayout.openDrawer(GravityCompat.START)
         }
 
         Btn_MAIN_Start.setOnClickListener{
@@ -24,5 +32,30 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        navView.setNavigationItemSelectedListener {
+            val intent: Intent? = when (it.itemId) {
+                R.id.menu_spacestation ->
+                    Intent(this, Statistics_Day_Activity::class.java)
+                R.id.menu_library ->
+                    Intent(this, LibraryActivity::class.java)
+                R.id.menu_reward ->
+                    Intent(this, Reward_Activity::class.java)
+                R.id.menu_setting -> null
+                else -> null
+            }
+            if (intent != null) {
+                startActivity(intent)
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener false
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
