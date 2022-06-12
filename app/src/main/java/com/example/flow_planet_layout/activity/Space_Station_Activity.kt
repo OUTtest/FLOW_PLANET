420e346e1e29c5ce795ae7cac8c84208ea8308bd
kjy00302 <kjy00302@gmail.com>
2022-06-12 19:17:39 +0900

    select category from db

--- app/src/main/java/com/example/flow_planet_layout/activity/Space_Station_Activity.kt
+++ app/src/main/java/com/example/flow_planet_layout/activity/Space_Station_Activity.kt
@@ -6,13 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.asLiveData
 import com.example.flow_planet_layout.R
-import org.w3c.dom.Text
+import com.example.flow_planet_layout.db.DBApplication
 
 class Space_Station_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_space_station)
+
+        val bookDao = (application as DBApplication).bookDao
+        var books: Map<Int, String>? = null
+
+        bookDao.getAll().asLiveData().observe(this) { list ->
+            books = list.associate { it.id to it.name }
+        }
 
         val Btn_Station_Back = findViewById<ImageButton>(R.id.Btn_Station_Back)
         val Btn_Station_Studian = findViewById<Button>(R.id.Btn_Station_Studian)
@@ -22,6 +31,7 @@ class Space_Station_Activity : AppCompatActivity() {
         val Btn_Station_min = findViewById<Button>(R.id.Btn_Station_Min)
         val Text_Station_Timer = findViewById<TextView>(R.id.Text_Station_Timer)
         var counter = 0
+        var bookId = -1
 
         Btn_Station_Back.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -35,13 +45,29 @@ class Space_Station_Activity : AppCompatActivity() {
         }
 
         Btn_Station_Start.setOnClickListener {
+            if (bookId == -1 ) return@setOnClickListener
             val intent = Intent(this, ExproreActivity::class.java)
             intent.putExtra("count", counter)
+            intent.putExtra("bookId", bookId)
             startActivity(intent)
             this.finish()
         }
 
         Btn_Station_Category.setOnClickListener {
+
+            if (books != null) {
+                AlertDialog.Builder(this@Space_Station_Activity).apply {
+                    if (bookId == -1)
+                        setTitle("카테고리를 선택하세요")
+                    else {
+                        setTitle("카테고리를 선택하세요\n(${books!![bookId]} 선택됨)")
+                    }
+                    val arr = books!!.toList()
+                    setItems(arr.map { it.second }.toTypedArray()) { dialog, which ->
+                        bookId = arr[which].first
+                    }
+                }.show()
+            }
 
         }
 
