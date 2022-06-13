7e287323ce0b223e32d27fc6b73b0adc04bf3a2d
Shun_Stain <lsa67@naver.com>
2022-06-13 13:29:33 +0900

    library_category_update

--- app/src/main/java/com/example/flow_planet_layout/activity/LibraryActivity.kt
+++ app/src/main/java/com/example/flow_planet_layout/activity/LibraryActivity.kt
@@ -1,14 +1,20 @@
 package com.example.flow_planet_layout.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flow_planet_layout.R
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flow_planet_layout.db.DBApplication
+import com.example.flow_planet_layout.db.entity.Book
 import com.example.flow_planet_layout.ui.BookShelfAdapter
+import kotlinx.coroutines.launch
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -17,8 +23,27 @@ class LibraryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_library)
         val bookDao = (application as DBApplication).bookDao
 
+        val Btn_Library_category = findViewById<Button>(R.id.Btn_library_category)
+
+
         findViewById<ImageButton>(R.id.btn_library_back).setOnClickListener{
             this.finish()
+        }
+
+        Btn_Library_category.setOnClickListener {
+            AlertDialog.Builder(this@LibraryActivity).apply{
+                val editText = EditText(this@LibraryActivity).apply {
+                    isSingleLine = true
+                }
+                setTitle("카테고리 입력")
+                setView(editText)
+                setPositiveButton("확인") { dialog, which ->
+                    lifecycleScope.launch {
+                        bookDao.put(Book(name = editText.text.toString()))
+                    }
+                }
+                setNegativeButton("취소") { _, _ ->}
+            }.show()
         }
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv_bookshelf)
